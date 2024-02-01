d14f9230c0c548650846467078245f9e81cbd6da
osman yaycioglu <osman.yaycioglu@gmail.com>
2024-02-01 16:09:23 +0300

    day  2 3

--- kubernetes/kubernete.cmd
+++ kubernetes/kubernete.cmd
@@ -9,3 +9,16 @@ minikube addons list
 kubectl get pods -n kube-system
 
 kind create cluster --config=kind-cluster.yaml
+
+kubectl describe nodes minikube
+
+kubectl describe pods kube-apiserver-minikube -n kube-system
+
+kubectl apply -f .\nginx-pod.yaml
+
+kubectl describe pods nginx-pod
+
+kubectl get pods -o wide
+
+kubectl exec -it nginx-pod -- sh
+
